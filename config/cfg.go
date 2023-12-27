a09781170b5707774ecf3924c5a647dbe0baa5dc
smallmenu <smallmenu@163.com>
2023-12-27 21:47:36 +0800

    update

--- config/cfg.go
+++ config/cfg.go
@@ -15,5 +15,5 @@ type cfg struct {
 		MaxAge     int    `json:"maxAge"`
 		MaxBackups int    `json:"maxBackups"`
 	} `json:"logger"`
-	Test map[string]string
+	Test map[string]string `json:"test"`
 }
